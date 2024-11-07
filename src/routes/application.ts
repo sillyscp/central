a3293b1f6f9224217a94911a567f8150898771a6
Jay <me@jayxtq.xyz>
2024-11-07 17:29:01 +0000

    feat: change text relating to discord message

--- src/routes/application.ts
+++ src/routes/application.ts
@@ -10,7 +10,7 @@ export default function (app: Express, client: Client) {
         const body = req.body as { userId: string };
         const guildMember = await new DiscordFetch(client).member("1279504339248877588", body.userId);
         if (!guildMember) return res.status(404).json({ error: "User not found" });
-        await guildMember.send(`Thank you for applying to Silly SCP as a Discord moderator.
+        await guildMember.send(`Thank you for applying to be staff at Silly SCP.
 The relevant people have been informed and given your application, this process may take some time so be patient.
 If you have any questions, feel free to make a ticket in the Discord and ask.`);
         return res.json({ success: true });
