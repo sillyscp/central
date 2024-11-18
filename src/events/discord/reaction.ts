e13dd18e15070db74cf577ecd0c00d617bebf612
Jay <me@jayxtq.xyz>
2024-11-18 17:34:40 +0000

    feat: message react logger

---
+++ src/events/discord/reaction.ts
@@ -0,0 +1,28 @@
+import {Client, EmbedBuilder, Interaction, TextChannel} from 'discord.js';
+import { commands } from '../..';
+import {DiscordFetch} from "../../utils/discord";
+
+export default async function (client: Client) {
+    client.on('messageReactionAdd', async (interaction) => {
+        if(!interaction.count || interaction.count > 1) return;
+        const users = await interaction.users.fetch();
+        if(users.size > 1) return;
+        if(users.first()!.bot) return;
+        const logsChannel = (await new DiscordFetch(interaction.client).channel("1295029840784920628"))! as TextChannel;
+        const thread = (await logsChannel.threads.fetch("1295030367572721807"))!;
+        const embed = new EmbedBuilder()
+            .setTitle("Reaction Added")
+            .setDescription(`A reaction was added to a message`)
+            .addFields({
+                name: "Message",
+                value: `[Jump to message](https://discord.com/channels/${interaction.message.guildId}/${interaction.message.channelId}/${interaction.message.id})`
+            }, {
+                name: "Reaction",
+                value: interaction.emoji.toString()
+            }, {
+                name: "User",
+                value: `<@${users.first()!.id}> (${users.first()!.id})`
+            });
+        await thread.send({ embeds: [embed] });
+    });
+}
